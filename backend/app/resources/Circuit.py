57cdcd8acb0a5b33ed2a5d85c002ec4268618d4c
tomazmm <tomaz.mesarec@gmail.com>
2021-08-25 17:41:06 +0200

    Capitalize file name

--- backend/app/resources/Circuit.py
+++ backend/app/resources/Circuit.py
@@ -8,11 +8,12 @@ class Circuit(Resource):
     def post(self):
         try:
             qasm_str = request.get_data().decode('utf-8')
-
             qc = QuantumCircuit.from_qasm_str(qasm_str)
+
             backend = Aer.get_backend(name="aer_simulator")
             job = execute(qc, backend)
 
             return job.result().get_counts()
         except Exception as e:
             return e.args[0], 400
+
