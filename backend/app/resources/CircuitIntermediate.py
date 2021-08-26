b450aa5e187e1eed2663d8f2208bdbe454061103
tomazmm <tomaz.mesarec@gmail.com>
2021-08-26 21:36:16 +0200

    Returning state vectors

--- backend/app/resources/CircuitIntermediate.py
+++ backend/app/resources/CircuitIntermediate.py
@@ -3,6 +3,8 @@ from flask_restful import Resource
 from flask import request
 from qiskit import QuantumCircuit, transpile
 from qiskit.providers.aer import Aer
+from qiskit.providers.aer.library import SaveStatevector
+from qiskit.providers.aer.library.default_qubits import default_qubits
 
 
 class CircuitIntermediate(Resource):
@@ -11,33 +13,35 @@ class CircuitIntermediate(Resource):
             qasm_str = request.get_data().decode('utf-8')
             circ = QuantumCircuit.from_qasm_str(qasm_str)
 
-            # circ = CircuitIntermediate._add_save_state_vectors(circ)
-
-            circ.save_vectorstate()
-
-            backend = Aer.get_backend(name="aer_simulator")
-
-            # circ.measure_active()
-
-            circ.draw("mpl", filename="image")
+            data_copy = circ.data.copy()
+            qubits = default_qubits(circ)
+            try:
+                i = 0
+                while data_copy:
+                    circuit_element = data_copy[i]
+                    if type(circuit_element[0]) is not SaveStatevector:
+                        sv = SaveStatevector(len(qubits), label=f"sv-{i}")
+                        data_copy.insert(i + 1, (sv, qubits, []))
+                    i += 1
+            except IndexError:
+                circ.data = data_copy
 
             # Transpile for simulator
-            circ = transpile(circ, backend)
+            simulator = Aer.get_backend('aer_simulator')
+            circ = transpile(circ, simulator)
 
             # Run and get statevector
-            result = backend.run(circ).result()
+            result_data = simulator.run(circ).result().data()
+
+            ret = {
+                "count": result_data["counts"],
+                "state_vectors": {}
+            }
+            for el in result_data:
+                if el == "counts": continue
+                ret["state_vectors"][el] = str(result_data[el])
+
+            return ret
 
         except Exception as e:
             return e.args[0], 400
-
-    @staticmethod
-    def _add_save_state_vectors(circuit: QuantumCircuit) -> QuantumCircuit:
-        print(*circuit.data, sep="\n")
-
-
-
-
-        return circuit
-
-
-
